cd22fc8714a7a9db00f6e567ae66b4971a4a6020
TobiasRDL <tlasso23@student.aau.dk>
2023-11-30 15:51:19 +0100

    updated database

--- src/main/main.c
+++ src/main/main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 int main (void){
-    staff_record staffs[10];
+    //staff_record staffs[10];
     resident_record residents[50];
     get_resident_record(residents);
+    get_resident_record_medicine(residents[0].id_key);
     //printf("\n\n%s",residents[0].first_name);
     //resident_record * resident = get_resident_record();
     //return number_of_residents_read;
