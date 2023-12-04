1f53c409ba1576c03ec24d5934ab869021527204
Jacob <jacob74447@gmail.com>
2023-12-04 15:36:06 +0100

    Udregning mellem faste stoffer og væsker samt eksempel på g/l to mg/ml

--- src/Calculator/Calculator.c
+++ src/Calculator/Calculator.c
@@ -1,6 +1,8 @@
 #include "Calculator.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 
 int medicine_calculator(){
     
@@ -10,35 +12,43 @@ int name_calculator(){
 
 }
 
-int unit_calculator(double dose, char rm_unit, char md_unit){
-    int rm_name, rm_strength;
-    int md_name, md_strength;
+double convert(double dose, const char *from_unit, const char *to_unit) {
+    struct Conversion {
+        const char *unit_pair;
+        double multiplier;
+    };
 
-    if (rm_unit == 'm' && md_unit == 'g') {
-        return dose / 1000.0; // mg to g
-    } else if (rm_unit == 'g' && md_unit == 'm') {
-        return dose * 1000.0; // g to mg
-    } else if (rm_unit == 'm' && md_unit == 'c') {
-        return dose * 1000.0; // mg to mcg
-    } else if (rm_unit == 'c' && md_unit == 'm') {
-        return dose / 1000.0; // mcg to mg
-    } else if (rm_unit == 'g' && md_unit == 'c') {
-        return dose * 1000000.0; // g to mcg
-    } else if (rm_unit == 'c' && md_unit == 'g') {
-        return dose / 1000000.0; // mcg to g
-    } else {
-        printf("Invalid conversion!\n");
-        return -1; // Invalid conversion
-    }
-    
-    
-    //her er styrken på den medicin patienten skal have og det som plejehjemmet har ens
-    if ((rm_name && rm_strength && rm_unit)== (md_name && md_strength && md_unit)) {
-        return 1;
-    }
+    struct Conversion conversions[] = {
+            { "mg->g", 0.001 }, { "g->mg", 1000.0 },
+            { "mg->mcg", 1000.0 }, { "mcg->mg", 0.001 },
+            { "g->mcg", 1000000.0 }, { "mcg->g", 0.000001 },
+            { "ml->l", 0.001 }, { "l->ml", 1000.0 },
+            { "ml->mcl", 1000.0 }, { "mcl->ml", 0.001 },
+            { "l->mcl", 1000000.0 }, { "mcl->l", 0.000001 },
+            {"mg/ml->g/l", 0.001}, {"mg/ml->mmol/ml", },
+            {"g/l->mg/ml", 1000.0}, {"g/l->mmol/ml", },
+            {"mmol/ml->g/l", }, {"mmol/ml->mg/ml", }
 
+            // Add more conversions as needed
+    };
+
+    for (int i = 0; i < sizeof(conversions) / sizeof(conversions[0]); ++i) {
+        char unit_pair_copy[strlen(conversions[i].unit_pair) + 1]; // Creating a copy of unit_pair
+        strcpy(unit_pair_copy, conversions[i].unit_pair);
+
+        char *from_token = strtok(unit_pair_copy, "->");
+        char *to_token = strtok(NULL, "->");
+
+        if (from_token != NULL && to_token != NULL) {
+            if (strcmp(from_unit, from_token) == 0 && strcmp(to_unit, to_token) == 0) {
+                double result = dose * conversions[i].multiplier;
+                printf("Convert %   lf %s to %s: %lf %s\n", dose, from_unit, to_unit, result, to_unit);
+                return result;            }
+        }
+    }
+    printf("Invalid conversion!\n");
+    return -1; // Invalid conversion
 }
-
 
 int strength_calculator(){
     
