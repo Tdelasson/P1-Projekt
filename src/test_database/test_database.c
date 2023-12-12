ee943f65169d84baa61775946f5bc6e73c1daa72
TobiasRDL <tlasso23@student.aau.dk>
2023-12-12 13:43:14 +0100

    udpate

--- src/test_database/test_database.c
+++ src/test_database/test_database.c
@@ -1,5 +1,7 @@
+#include "../infusion/infusion.h"
 #include <assert.h>
 
 int main(void){
-    //assert(get_patient_journal(patients[]) == "Adam");
+    /*resident_medications medications[MAX_MEDICATIONS];
+    assert((get_resident_record_medicine(medications,10001)) == 5);*/
 }
